1afa4c8b0362356d1280f803eb47dc4aaf7b2e3b
Alexander <alexdiro518@gmail.com>
2024-04-10 14:27:14 -0600

    AuthTitles por AuthHeadings

---
+++ src/components/auth/AuthTitles.tsx
@@ -0,0 +1,19 @@
+type Props = {
+    title: string;
+    info?: string;
+}
+export const AuthTitles = ({ title, info }: Props) => {
+
+    return (
+        <div className="my-4 text-center">
+            <h1 style={{ color: "white" }}>{title}</h1>
+            {
+                info && (
+                    <p style={{ color: "white" }} className="mt-4">
+                        {info}
+                    </p>
+                )
+            }
+        </div>
+    )
+}
