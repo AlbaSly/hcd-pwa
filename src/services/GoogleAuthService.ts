254dfec1544c6a2ec78cecf5a5e40e486dfe4809
Raxel Arias <dev.raxelarias@gmail.com>
2024-04-10 04:38:01 -0600

    feat: Crear servicio para manejar el clientID de Google

---
+++ src/services/GoogleAuthService.ts
@@ -0,0 +1,8 @@
+export class GoogleAuthService {
+    
+    static getClientId() {
+        const CLIENT_ID = "618770095508-2p1e2blqipq9tsb9tl5g5rccso1tmone.apps.googleusercontent.com";
+        return CLIENT_ID;
+    }
+
+}
