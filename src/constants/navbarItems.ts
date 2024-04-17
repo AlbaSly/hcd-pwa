8c29a536472339d5adcb06031377bd9b163a975b
Raxel Arias <dev.raxelarias@gmail.com>
2024-04-17 04:52:21 -0600

    refactor: Modificar constante navbaritems

--- src/constants/navbarItems.ts
+++ src/constants/navbarItems.ts
@@ -29,6 +29,20 @@ export const navbarItems: INavbarItem[] = [
     }
 ]
 
+export const getSortedNavbarItems = (): INavbarItem[] => {
+    const copy = [...navbarItems];
+
+    const indexInicio = copy.findIndex(item => item.label === 'Inicio');
+
+    // Mueve el elemento con label "Inicio" al principio del array
+    if (indexInicio !== -1) {
+        const inicioItem = copy.splice(indexInicio, 1)[0];
+        copy.unshift(inicioItem);
+    }
+
+    return copy;
+}
+
 /**
  * Interfaz que representa la estructura de un elemento de la barra de navegación
  */
